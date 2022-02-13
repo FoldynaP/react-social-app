fb61158813104f28f8ce89f6f642fb3a4ee3abc2
Petr Foldyna <petr.foldyna12@gmail.com>
2022-02-13 23:58:09 +0100

    Started with router links

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
-  return <Profile/>;
+  return(
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/home">
+            <Home/>
+          </Route>
+          <Route  exact path="/login">
+            <Login/>
+          </Route>
+          <Route exact path="/register">
+            <Register/>
+          </Route>
+          <Route exact path="/profile">
+            <Profile/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+ )
+
 }
 
 export default App;
